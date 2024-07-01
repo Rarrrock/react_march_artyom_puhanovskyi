6f2b080d76479cf85c2ed41995be02dc7a8a20dc
Артём Рассветный <artemius.morningstar@gmail.com>
2024-07-01 11:45:07 +0300

    Lesson_3_basicHooks-useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {FC, useState} from 'react';
 import './App.css';
 
-const App = () =>  {
+const App: FC = () =>  {
+    let [number, setNumber] = useState<number>(0);
 
+  // let number = 0;
+  const increment = () => {
+    setNumber((prevState => prevState + 1));
+    console.log(number);
+  }
   return (
     <div>
-
+      <h2>{number}</h2>
+      <button onClick={increment}> increment</button>
     </div>
   );
 }
