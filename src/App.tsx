a4aaee8a1bb084d142de8c805ec2a935c712e86f
Артём Рассветный <artemius.morningstar@gmail.com>
2024-07-01 17:20:38 +0300

    Lesson_3_basicHooks-3-useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {FC, useEffect, useRef, useState} from 'react';
 import './App.css';
 
-const App = () =>  {
+const App: FC = () =>  {
+
+  let [x, setX] = useState<number>(0);
+  let y = useRef(0);
+
+  useEffect(() => {
+    console.log('work');
+
+    return () => {
+      console.log('!!!');
+    }
+  },[x]);
 
   return (
     <div>
+      <button onClick={() => {
+        setX(prevState => ++prevState);
+      }}>change x</button>
 
+      <button onClick={() => {
+        y.current++;
+        console.log(y.current);
+      }}>change y</button>
     </div>
   );
 }
