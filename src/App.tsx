65f9240477682780185ea83eb0209713fb9d7b5e
Артём Рассветный <artemius.morningstar@gmail.com>
2024-07-01 12:39:23 +0300

    Lesson_3_basicHooks-2-useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import './App.css';
 
-const App = () =>  {
+const App: FC = () =>  {
+
+    let [x, setX] = useState<number>(0);
+
+  useEffect(() => {
+    console.log('work');
+
+    return () => {
+        console.log('!!!');
+    }
+  },[x])
 
   return (
     <div>
-
+      <button onClick={() => {
+        setX(prevState => ++prevState);
+      }}>
+        change
+      </button>
     </div>
   );
 }
