657a92619c6625145a3da7a42a93218759ddf520
Артём Рассветный <artemius.morningstar@gmail.com>
2024-07-02 13:33:29 +0300

    Lesson_3_basicHooks-3-useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, {FC,useReducer,} from 'react';
 import './App.css';
 
-const App = () =>  {
+const reducer = (
+    state: number,
+    action: {type: string, payload:number}) => {
+    switch (action.type) {
+        case 'add':
+            return state + action.payload;
+        case 'remove':
+            return state + action.payload;
+    }
+    return state;
+}
+
+const App: FC = () =>  {
+    const [counter, dispatch] = useReducer(reducer, 0);
 
   return (
     <div>
-
+        <h2>{counter}</h2>
+        <button
+            onClick={() => {
+                dispatch({type: 'add', payload: 10});
+        }}
+        >increment
+        </button>
+        <hr/>
+        <button
+            onClick={() => {
+                dispatch({type: 'add', payload: -20});
+            }}
+        >decrement
+        </button>
     </div>
   );
 }
