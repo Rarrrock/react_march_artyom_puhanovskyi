11e10cf009d9c68a5b058eb25944c814c4f1594c
Артём Рассветный <artemius.morningstar@gmail.com>
2024-07-08 16:51:39 +0300

    Lesson_5/3-useForm+validation

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -9,18 +9,24 @@ type IFormType = {
 
 const FormComponent = () => {
 
-    let {register, handleSubmit} = useForm<IFormType>();
+    let {formState:{errors, isValid}, register, handleSubmit} = useForm<IFormType>();
 
     let formSubmitCustomHandler = (data: IFormType) => {
         console.log(data);
     };
     return (
         <div>
+            {errors.age && <div>{errors.age.message} {isValid+''}</div>}
             <form onSubmit={handleSubmit(formSubmitCustomHandler)}>
                 <input type="text" {...register('username')}/>
                 <input type="text" {...register('password')}/>
-                <input type="number" {...register('age')}/>
-                <button>send</button>
+                <input type="number" {...register('age', {
+                    required: true,
+                    valueAsNumber: true,
+                    min: {value: 1, message: 'age too small'},
+                    max: {value: 132, message: 'age too big'}
+                    })}/>
+                <button disabled={!isValid}>send</button>
             </form>
         </div>
     );
