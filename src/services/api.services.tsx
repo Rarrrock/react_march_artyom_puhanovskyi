ee56f673369d31b52fcb36e3d18531ba460f4f00
Артём Рассветный <artemius.morningstar@gmail.com>
2024-07-24 16:36:44 +0300

    lesson_7_HW_(6+pagination) WORK! 3 + typisation When press on any post, we return to first page.

--- src/services/api.services.tsx
+++ src/services/api.services.tsx
@@ -1,45 +1,48 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
+import {IUser} from "../models/IUser";
+import {IPost} from "../models/IPost";
+import {IComment} from "../models/IComment";
 
 const API_URL = 'https://jsonplaceholder.typicode.com';
 
-export const getUsers = (skip = 0) => {
-    return axios.get(`${API_URL}/users`, {
+export const getUsers = (skip = 0): Promise<AxiosResponse<IUser[]>> => {
+    return axios.get<IUser[]>(`${API_URL}/users`, {
         params: { _start: skip, _limit: 30 }
     });
 };
 
-export const getUser = (id: number) => {
-    return axios.get(`${API_URL}/users/${id}`);
+export const getUser = (id: number): Promise<AxiosResponse<IUser>> => {
+    return axios.get<IUser>(`${API_URL}/users/${id}`);
 };
 
-export const getUserPosts = (userId: number) => {
-    return axios.get(`${API_URL}/posts`, {
+export const getUserPosts = (userId: number): Promise<AxiosResponse<IPost[]>> => {
+    return axios.get<IPost[]>(`${API_URL}/posts`, {
         params: { userId }
     });
 };
 
-export const getPosts = (skip = 0) => {
-    return axios.get(`${API_URL}/posts`, {
+export const getPosts = (skip = 0): Promise<AxiosResponse<IPost[]>> => {
+    return axios.get<IPost[]>(`${API_URL}/posts`, {
         params: { _start: skip, _limit: 30 }
     });
 };
 
-export const getPost = (id: number) => {
-    return axios.get(`${API_URL}/posts/${id}`);
+export const getPost = (id: number): Promise<AxiosResponse<IPost>> => {
+    return axios.get<IPost>(`${API_URL}/posts/${id}`);
 };
 
-export const getPostComments = (postId: number) => {
-    return axios.get(`${API_URL}/comments`, {
+export const getPostComments = (postId: number): Promise<AxiosResponse<IComment[]>> => {
+    return axios.get<IComment[]>(`${API_URL}/comments`, {
         params: { postId }
     });
 };
 
-export const getComments = (skip = 0) => {
-    return axios.get(`${API_URL}/comments`, {
+export const getComments = (skip = 0): Promise<AxiosResponse<IComment[]>> => {
+    return axios.get<IComment[]>(`${API_URL}/comments`, {
         params: { _start: skip, _limit: 30 }
     });
 };
 
-export const getComment = (id: number) => {
-    return axios.get(`${API_URL}/comments/${id}`);
+export const getComment = (id: number): Promise<AxiosResponse<IComment>> => {
+    return axios.get<IComment>(`${API_URL}/comments/${id}`);
 };
