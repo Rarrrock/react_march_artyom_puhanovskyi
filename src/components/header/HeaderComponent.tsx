202ed564dd07a9dbec3e45e330dcfa023a78754d
Артём Рассветный <artemius.morningstar@gmail.com>
2024-07-26 12:42:00 +0300

    lesson_9_auth_api_implementation

---
+++ src/components/header/HeaderComponent.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {NavLink} from "react-router-dom";
+import styles from './HeaderComponent.module.css'
+
+const HeaderComponent = () => {
+    return (
+        <div className={styles.xxx}>
+            <ul>
+                <li><NavLink to={'/'}>auth page</NavLink></li>
+                <li><NavLink to={'/registration'}>registration page</NavLink></li>
+            </ul>
+
+
+            <hr/>
+        </div>
+    );
+};
+
+export default HeaderComponent;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import AuthPage from "./pages/AuthPage";
+import RegPage from "./pages/RegPage";
 
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-    <App />
+    document.getElementById('root') as HTMLElement
 );
 
+let router = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainLayout/>,
+        errorElement: <h1>aaaaaaaa errorrr!</h1>,
+        children: [
+            {
+                index: true, element: <AuthPage/>
+            },
+            {
+                path: '/registration', element: <RegPage/>
+            }
+        ]
+
+    }
+]);
+root.render(
+    <RouterProvider router={router}/>
+);
