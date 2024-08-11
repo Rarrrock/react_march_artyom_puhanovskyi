30f519c876e700bc7912aacf5120d3d75ac54601
Артём Рассветный <artemius.morningstar@gmail.com>
2024-08-11 15:02:37 +0300

    lesson_10_redux_base

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,40 @@
 import React from 'react';
 import './App.css';
+import {Provider} from "react-redux"; // Импортируем Provider
+import {store} from "./redux/store"; // Импортируем созданное хранилище
+import {decrement, increment, incrementByAmount, useAppDispatch, useAppSelector} from "./redux/store";
 
-const App = () =>  {
+const App = () => {
+    let {value} = useAppSelector(state => state.counter1SliceState);
 
-  return (
-    <div>
+    let dispatch = useAppDispatch();
 
-    </div>
-  );
+    return (
+        <div>
+            <h2>{value}</h2>
+            <button onClick={() => {
+                dispatch(increment());
+            }}>
+                increment by 1
+            </button>
+            <button
+                onClick={() => {
+                    dispatch(decrement());
+                }}
+            >
+                decrement by 1
+            </button>
+            <button onClick={() => {
+                dispatch(incrementByAmount(100));
+            }}>
+                increment by 100
+            </button>
+        </div>
+    );
 }
 
-export default App;
+export default () => (
+    <Provider store={store}>  {/* Оборачиваем App в Provider */}
+        <App />
+    </Provider>
+);
---
+++ src/redux/store.ts
@@ -0,0 +1,45 @@
+import {configureStore, createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {useDispatch, useSelector} from "react-redux";
+
+type CounterStateType = {
+    value: number
+}
+
+let initStateCounterSlice1: CounterStateType = {
+    value: 0,
+}
+
+let counterSlice1 = createSlice({
+    name: 'counter1SliceName',
+    initialState: initStateCounterSlice1,
+    reducers: {
+        increment: (state) => {
+            state.value = state.value + 1;
+        },
+        decrement: (state) => {
+            state.value = state.value - 1;
+        },
+        incrementByAmount: (state, action: PayloadAction<number>) => {
+            state.value = state.value + action.payload;
+        }
+    }
+});
+
+export const {
+    increment,
+    decrement,
+    incrementByAmount
+} = counterSlice1.actions;
+
+
+export const store = configureStore({
+    reducer: {
+        counter1SliceState: counterSlice1.reducer
+    }
+});
+
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+
+export type RootState = ReturnType<typeof store.getState>;
+export const useAppSelector = useSelector.withTypes<RootState>();
