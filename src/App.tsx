a58c915970ca25225266b396f7e3e47a197d2ade
Артём Рассветный <artemius.morningstar@gmail.com>
2024-08-11 15:05:57 +0300

    lesson_10_redux_base_2

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,35 @@
 import React from 'react';
 import './App.css';
-import {Provider} from "react-redux"; // Импортируем Provider
-import {store} from "./redux/store"; // Импортируем созданное хранилище
 import {decrement, increment, incrementByAmount, useAppDispatch, useAppSelector} from "./redux/store";
 
 const App = () => {
-    let {value} = useAppSelector(state => state.counter1SliceState);
+  let {value} = useAppSelector(state => state.counter1SliceState);
 
-    let dispatch = useAppDispatch();
+  let dispatch = useAppDispatch();
 
-    return (
-        <div>
-            <h2>{value}</h2>
-            <button onClick={() => {
-                dispatch(increment());
-            }}>
-                increment by 1
-            </button>
-            <button
-                onClick={() => {
-                    dispatch(decrement());
-                }}
-            >
-                decrement by 1
-            </button>
-            <button onClick={() => {
-                dispatch(incrementByAmount(100));
-            }}>
-                increment by 100
-            </button>
-        </div>
-    );
+  return (
+      <div>
+
+        <h2>{value}</h2>
+        <button onClick={() => {
+          dispatch(increment());
+        }}>
+          increment by 1
+        </button>
+        <button
+            onClick={() => {
+              dispatch(decrement());
+            }}
+        >
+          decrement by 1
+        </button>
+        <button onClick={() => {
+          dispatch(incrementByAmount(100));
+        }}>
+          increment by 100
+        </button>
+      </div>
+  );
 }
 
-export default () => (
-    <Provider store={store}>  {/* Оборачиваем App в Provider */}
-        <App />
-    </Provider>
-);
+export default App;
